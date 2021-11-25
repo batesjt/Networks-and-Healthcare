790c99ce2b67d5f03b09a8308726b5f07b02c96f
Jonah Bates <34543922+batesjt@users.noreply.github.com>
2021-11-25 15:28:30 +0000

    Add files via upload

---
+++ Networks and Healthcare/random_hgraph_generator.py
@@ -0,0 +1,3 @@
+import pandas as pd
+
+dataset = pd.read_csv("comorb_hypergraph.txt", delimiter = ",", header=None)
---
+++ Networks and Healthcare/test.py
@@ -0,0 +1,5 @@
+import main_approx_ver2_wip as mochy
+
+input = "hypergraphTest.txt"
+car = mochy.h_motifs_count(input)
+print("nice one")
